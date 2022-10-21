d31bf2246a0582a0a0d570d176d452cddaafdf6d
SpencerAung <10285698+SpencerAung@users.noreply.github.com>
2022-10-21 20:36:01 +0900

    Add link to build page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react"
+import  Link  from 'next/link';
 import { useSession, signIn, signOut } from "next-auth/react"
 
 import Button from '../components/Button';
@@ -22,9 +23,11 @@ function Nav() {
 
   return (
     <nav className={"w-100 py-2 px-4 flex items-center justify-between"}>
-      <div>
-        <span className="text-xl font-bold">Dinoku</span>
+      <div className="flex items-end gap-4">
+        <Link href="/"><a className="text-xl font-bold">Dinoku</a></Link>
+        <Link href="/builder"><a className="font-semibold">Build</a></Link>
       </div>
+
       <div className="flex items-center gap-4">
         <div className="flex items-center gap-1">
           {image && (
